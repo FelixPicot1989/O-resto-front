9807162793025c4280cf5cb57c7fa72813dd08e2
MaximeGuyot1 <guyotmaxime02@gmail.com>
2023-06-08 09:41:52 +0200

    config for format on save with ESLint and Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,37 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        printWidth: 120,
+      },
+    ],
+  },
+};
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,10 @@
-import React from 'react'
-import { useState } from 'react';
-import {
-  FaBars,
-  FaTimes,
-  FaFacebook,
-  FaInstagram,
-  FaTripadvisor
-} from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaBars, FaTimes, FaFacebook, FaInstagram, FaTripadvisor } from 'react-icons/fa';
 
 import logoOresto from '../../assets/logo-oresto.png';
 import './Navbar.scss';
 
-const Navbar = () => {
+function Navbar() {
   const [nav, setNav] = useState(false);
   const handleClick = () => {
     setNav(!nav);
@@ -19,68 +12,52 @@ const Navbar = () => {
 
   return (
     <nav className="Navbar">
-        <img src={logoOresto} alt='logo-oresto' className='logo-oresto' />
-        <h1 className={!nav
-            ? 'title-hidden'
-            : 'title'}>O'Resto</h1>
-        {/* Desktop menu */}
-        <div className="Navbar-desktop">
-          <ul className="Navbar-list" >
-            <li className="Navbar-list-item">
-                Accueil
-            </li>
-            <li className="Navbar-list-item">
-                La carte
-            </li>
-            <li className="Navbar-list-item">
-                Réserver/Contact
-            </li>
-            <li className="Navbar-list-item">
-                Donner votre avis
-            </li>
-          </ul>
-          <div className='login-social-desktop'>
-            <button className='btn-login' type='button'>Connexion</button>
-            <div className='social-desktop'>
-              <FaFacebook />
-              <FaInstagram />
-              <FaTripadvisor />
-            </div>
+      <img src={logoOresto} alt="logo-oresto" className="logo-oresto" />
+      <h1 className={!nav ? 'title-hidden' : 'title'}>O'Resto</h1>
+      {/* Desktop menu */}
+      <div className="Navbar-desktop">
+        <ul className="Navbar-list">
+          <li className="Navbar-list-item">Accueil</li>
+          <li className="Navbar-list-item">La carte</li>
+          <li className="Navbar-list-item">Réserver/Contact</li>
+          <li className="Navbar-list-item">Donner votre avis</li>
+        </ul>
+        <div className="login-social-desktop">
+          <button className="btn-login" type="button">
+            Connexion
+          </button>
+          <div className="social-desktop">
+            <FaFacebook />
+            <FaInstagram />
+            <FaTripadvisor />
           </div>
         </div>
-        {/* Bouton burger */}
-        <div onClick={handleClick} className="Navbar-burger">
-          {!nav ? <FaBars /> : <FaTimes />}
-        </div>
-        {/* Mobile menu */}
-        <div className={!nav
-            ? 'Navbar-mobile-hidden'
-            : 'Navbar-mobile'}>
-          <ul>
-            <li className="Navbar-mobile-item">
-                Accueil
-            </li>
-            <li className="Navbar-mobile-item">
-                La carte
-            </li>
-            <li className="Navbar-mobile-item">
-                Réserver/Contact
-            </li>
-            <li className="Navbar-mobile-item">
-                Donner votre avis
-            </li>
-          </ul>
-          <div className='login-social-mobile'>
-            <button className='btn-login' type='button'>Connexion</button>
-            <div className='social-media'>
-              <FaFacebook />
-              <FaInstagram />
-              <FaTripadvisor />
-            </div>
+      </div>
+      {/* Bouton burger */}
+      <div onClick={handleClick} className="Navbar-burger">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile menu */}
+      <div className={!nav ? 'Navbar-mobile-hidden' : 'Navbar-mobile'}>
+        <ul>
+          <li className="Navbar-mobile-item">Accueil</li>
+          <li className="Navbar-mobile-item">La carte</li>
+          <li className="Navbar-mobile-item">Réserver/Contact</li>
+          <li className="Navbar-mobile-item">Donner votre avis</li>
+        </ul>
+        <div className="login-social-mobile">
+          <button className="btn-login" type="button">
+            Connexion
+          </button>
+          <div className="social-media">
+            <FaFacebook />
+            <FaInstagram />
+            <FaTripadvisor />
           </div>
         </div>
-      </nav>
-  )
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
