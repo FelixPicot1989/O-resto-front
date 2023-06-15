0163be3438d40340cbbfcaef0c7776b18f73c677
ErwannRousseau <erwann.rousseau@icloud.com>
2023-06-15 19:39:07 +0200

    drinks display management

--- src/components/ListDrinks/ListDrinks.jsx
+++ src/components/ListDrinks/ListDrinks.jsx
@@ -2,21 +2,21 @@ import PropTypes from 'prop-types';
 import './ListDrinks.scss';
 
 function ListDrinks({ drinks }) {
-  console.log(typeof drinks);
+  console.log(drinks);
   return (
     <div className="ListDrinks">
       {/* Object.keys(drinks) renvoie un tableau de toutes les clés de l'objet drinks. */}
       {Object.keys(drinks).map((category) => (
         <div className="category">
-          <h2 className="category-title">{category}</h2>
+          <h2 className="category-name">{category}</h2>
           <ul className="drinks-list" key={category}>
             {drinks[category].map((drink) => (
               <li className="drinks-item" key={drink.id}>
                 <div>
-                  <p>{drink.name}</p>
-                  <p>{drink.description}</p>
+                  <p className="drinks-name">{drink.name}</p>
+                  <p className="drinks-description">{drink.description}</p>
                 </div>
-                <p className="price">{drink.price} €</p>
+                <p className="drinks-price">{drink.price}&nbsp;€</p>
               </li>
             ))}
           </ul>
